113b1f5829c120a08f1418d192884a213f9e3e4c
allandereal <ahullan@gmail.com>
2020-04-01 19:48:26 +0300

    remove redundant classes from switch

--- resources/views/tailwind-switch.blade.php
+++ resources/views/tailwind-switch.blade.php
@@ -12,7 +12,7 @@
     <div class="p-1 border-2 border-solid border-gray-300 rounded-full">
         <label x-data="toggleSwitch(false)" class="relative inline-block w-16 h-9">
             <input :checked="isOn" id="on_off" name="on_off" class="opacity-0 w-0 h-0" type="checkbox">
-            <span @click="changeStatus" :class="{ 'bg-blue-200': isOn }" class="absolute cursor-pointer inset-0 bg-gray-200 rounded-full">
+            <span @click="changeStatus" :class="{ 'bg-blue-200': isOn, 'bg-gray-200': !isOn }" class="absolute cursor-pointer inset-0 rounded-full">
                 <span :class="{ 'translate-x-6 ml-1 bg-blue-500': isOn }" class="absolute inline-block left-0 top-0 w-9 h-9 bg-gray-500 rounded-full transition-all duration-300 transform"></span>
             </span>
         </label>
@@ -20,7 +20,7 @@
     <div class="ml-10">
         <label x-data="toggleSwitch(true)" class="relative inline-block w-16 h-10">
             <input :checked="isOn" id="on_off" name="on_off" class="opacity-0 w-0 h-0" type="checkbox">
-            <span @click="changeStatus" :class="{ 'bg-blue-500': isOn }" class="absolute cursor-pointer inset-0 p-1 bg-gray-300 rounded-full">
+            <span @click="changeStatus" :class="{ 'bg-blue-500': isOn, 'bg-gray-300': !isOn }" class="absolute cursor-pointer inset-0 p-1 rounded-full">
                 <span :class="{ 'translate-x-6': isOn }" class="inline-block w-8 h-8 bg-white rounded-full transition-all duration-300 transform"></span>
             </span>
         </label>
